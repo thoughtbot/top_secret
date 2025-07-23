405def185b59eaba7dea95bf3e53ea035cf23b17
Steve Polito <stevepolito@hey.com>
2025-07-23 07:43:50 -0400

    Initial commit
    Ran bundle gem `top_secret`, and followed the prompts.
    
    - Enabled RSpec
    - Enabled GitHub CI
    - Enabled Standard
    - Selected the default license (MIT)

---
+++ lib/top_secret.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "top_secret/version"
+
+module TopSecret
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/top_secret_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TopSecret do
+  it "has a version number" do
+    expect(TopSecret::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
