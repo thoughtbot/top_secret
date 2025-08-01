d5b9cc8e7609bc3e3fc6d42112abfe56a54b1076
Steve Polito <stevepolitodesign@users.noreply.github.com>
2025-08-01 10:35:30 -0400

    Filter emails from free text (#8)
    Closes #4
    
    Introduce basic interface and functionality for filtering email
    addresses from free text based on `URI::MailTo::EMAIL_REGEXP`.
    
    We deliberately keep this interface simple. Once a more concrete pattern
    emerges, we can create new abstractions.

--- lib/top_secret.rb
+++ lib/top_secret.rb
@@ -3,6 +3,43 @@
 require_relative "top_secret/version"
 
 module TopSecret
+  # Modified from URI::MailTo::EMAIL_REGEXP
+  EMAIL_REGEX = %r{[a-zA-Z0-9.!\#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*}
+
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Text
+    def initialize(input)
+      @input = input
+      @output = input.dup
+    end
+
+    def self.filter(input)
+      new(input).filter
+    end
+
+    def filter
+      emails = input.scan(EMAIL_REGEX)
+
+      emails.uniq.each.with_index(1) do |email, index|
+        filter = "EMAIL_#{index}"
+        output.gsub! email, "[#{filter}]"
+      end
+
+      Result.new(input, output)
+    end
+
+    private
+
+    attr_reader :output, :input
+  end
+
+  class Result
+    attr_reader :input, :output
+
+    def initialize(input, output)
+      @input = input
+      @output = output
+    end
+  end
 end
