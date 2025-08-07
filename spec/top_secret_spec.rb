e155cbfccc0e8d977b839a9191fdd35f7d58fb46
Steve Polito <stevepolitodesign@users.noreply.github.com>
2025-08-07 13:22:42 -0400

    Support custom filters (#31)
    Closes #25
    
    This commit allows us to globally override existing filters:
    
    ```ruby
    TopSecret.configure do |config|
      config.default_filters.email_filter = TopSecret::Filters::Regex.new(label: "EMAIL_ADDRESS", regex: TopSecret::EMAIL_REGEX)
    end
    ```
    
    As well as create new ones:
    
    ```ruby
    TopSecret.configure do |config|
      config.default_filters.passport = TopSecret::Filters::Regex.new(label: "PASSPORT", regex: /\b[A-Z0-9]{6,9}\b/i)
    end
    
    result = TopSecret::Text.filter("A123456") # => #<TopSecret::Result @input="A123456", @mapping={PASSPORT_1: "A123456"}, @output="[PASSPORT_1]">
    ```
    
    You can also disable a default filter altogether:
    
    ```ruby
    TopSecret.configure do |config|
      config.default_filters.email_filter = nil
    end
    ```
    
    This commit also supports overriding and creating new filters via a
    `filters` option:
    
    ```ruby
    TopSecret::Text.filter(input, filters: {
      email_filter: TopSecret::Filters::Regex.new(
        label: "EMAIL_ADDRESS",
        regex: /user\[at\]example\.com/
      )
    })
    ```
    
    You can also ignore a default filter altogether:
    
    ```ruby
    TopSecret::Text.filter(input, filters: {
      email_filter: nil
    }
    ```
    
    The current default filters are:
    
    1. `credit_card_filter`
    2. `email_filter`
    3. `phone_number_filter`
    4. `ssn_filter`
    5. `people_filter`
    6. `location_filter`

--- spec/top_secret_spec.rb
+++ spec/top_secret_spec.rb
@@ -5,9 +5,17 @@ RSpec.describe TopSecret do
     expect(TopSecret::VERSION).not_to be nil
   end
 
-  it "is configurable" do
+  it "has default configuration values" do
     expect(TopSecret.model_path).to eq("ner_model.dat")
     expect(TopSecret.min_confidence_score).to eq(0.5)
+    expect(TopSecret.default_filters).to match(
+      credit_card_filter: an_instance_of(TopSecret::Filters::Regex),
+      email_filter: an_instance_of(TopSecret::Filters::Regex),
+      phone_number_filter: an_instance_of(TopSecret::Filters::Regex),
+      ssn_filter: an_instance_of(TopSecret::Filters::Regex),
+      people_filter: an_instance_of(TopSecret::Filters::NER),
+      location_filter: an_instance_of(TopSecret::Filters::NER)
+    )
   end
 end
 
@@ -18,34 +26,99 @@ RSpec.describe "TopSecret Configuration" do
     allow(Mitie::NER).to receive(:new).and_return(ner)
   end
 
-  it "initializes Mitie::NER with the default model_path" do
+  it "initializes Mitie::NER with the default TopSecret.model_path value" do
     TopSecret::Text.filter("")
 
     expect(Mitie::NER).to have_received(:new).with(TopSecret.model_path)
   end
 
-  context "when the model_path is configured" do
-    it "initializes Mitie::NER with model_path" do
-      with_configuration do
-        TopSecret.configure do |config|
-          config.model_path = "custom_path.dat"
-        end
-
-        TopSecret::Text.filter("")
-
-        expect(Mitie::NER).to have_received(:new).with("custom_path.dat")
+  context "when the TopSecret.model_path configuration is overridden" do
+    it "initializes Mitie::NER with the custom value" do
+      original = TopSecret.model_path
+      TopSecret.configure do |config|
+        config.model_path = "custom_path.dat"
       end
+
+      TopSecret::Text.filter("")
+
+      expect(Mitie::NER).to have_received(:new).with("custom_path.dat")
+    ensure
+      TopSecret.configure { _1.model_path = original }
     end
   end
 
-  def with_configuration
-    original = TopSecret.config.dup
+  it "allows TopSecret.default_filters to be overridden" do
+    original = TopSecret.default_filters.email_filter
 
-    yield
-  ensure
     TopSecret.configure do |config|
-      original.each_pair { |k, v| config.send("#{k}=", v) }
+      config.default_filters.email_filter = TopSecret::Filters::Regex.new(
+        label: "email",
+        regex: /user\[at\]example\.com/
+      )
     end
+
+    result = TopSecret::Text.filter("user[at]example.com")
+
+    expect(result.output).to eq("[email_1]")
+    expect(result.mapping).to eq({
+      email_1: "user[at]example.com"
+    })
+  ensure
+    TopSecret.configure { _1.default_filters.email_filter = original }
+  end
+
+  it "allows TopSecret.default_filters to be ignored" do
+    original = TopSecret.default_filters.email_filter
+
+    TopSecret.configure do |config|
+      config.default_filters.email_filter = nil
+    end
+
+    result = TopSecret::Text.filter("user@example.com")
+
+    expect(result.output).to eq("user@example.com")
+    expect(result.mapping).to eq({})
+  ensure
+    TopSecret.configure { _1.default_filters.email_filter = original }
+  end
+
+  it "respects new regex filters" do
+    TopSecret.configure do |config|
+      config.default_filters.passport = TopSecret::Filters::Regex.new(
+        label: "PASSPORT",
+        regex: /\b[A-Z0-9]{6,9}\b/i
+      )
+    end
+
+    result = TopSecret::Text.filter("A123456")
+
+    expect(result.output).to eq("[PASSPORT_1]")
+    expect(result.mapping).to eq({
+      PASSPORT_1: "A123456"
+    })
+  ensure
+    TopSecret.configure { _1.default_filters.delete(:passport) }
+  end
+
+  it "respects new NER filters" do
+    ip_address = build_entity(text: "192.168.1.1", tag: :ip_address)
+    stub_ner_entities(ip_address)
+
+    TopSecret.configure do |config|
+      config.default_filters.passport = TopSecret::Filters::NER.new(
+        label: "IP_ADDRESS",
+        tag: :ip_address
+      )
+    end
+
+    result = TopSecret::Text.filter("192.168.1.1")
+
+    expect(result.output).to eq("[IP_ADDRESS_1]")
+    expect(result.mapping).to eq({
+      IP_ADDRESS_1: "192.168.1.1"
+    })
+  ensure
+    TopSecret.configure { _1.default_filters.delete(:passport) }
   end
 end
 
@@ -67,9 +140,10 @@ end
 
 RSpec.describe TopSecret::Text do
   describe ".filter" do
+    let(:ralph) { build_entity(text: "Ralph", tag: :person) }
+    let(:boston) { build_entity(text: "Boston", tag: :location) }
+
     before do
-      ralph = build_entity(text: "Ralph", tag: :person)
-      boston = build_entity(text: "Boston", tag: :location)
       stub_ner_entities(ralph, boston)
     end
 
@@ -103,6 +177,203 @@ RSpec.describe TopSecret::Text do
         LOCATION_1: "Boston"
       })
       expect(result.input).to eq(input)
+    end
+
+    context "when the filters option is passed" do
+      it "overrides existing regex filters" do
+        input = <<~TEXT
+          My name is Ralph
+          My location is Boston
+          My email address is user[at]example.com
+          My credit card numbers are 4242-4242-4242-4242 and 4141414141414141
+          My social security number is 123-45-6789
+          My phone number is 555-555-5555
+        TEXT
+
+        result = TopSecret::Text.filter(input, filters: {
+          email_filter: TopSecret::Filters::Regex.new(
+            label: "EMAIL_ADDRESS",
+            regex: /user\[at\]example\.com/
+          )
+        })
+
+        expect(result.output).to eq(<<~TEXT)
+          My name is [PERSON_1]
+          My location is [LOCATION_1]
+          My email address is [EMAIL_ADDRESS_1]
+          My credit card numbers are [CREDIT_CARD_1] and [CREDIT_CARD_2]
+          My social security number is [SSN_1]
+          My phone number is [PHONE_NUMBER_1]
+        TEXT
+        expect(result.mapping).to eq({
+          EMAIL_ADDRESS_1: "user[at]example.com",
+          CREDIT_CARD_1: "4242-4242-4242-4242",
+          CREDIT_CARD_2: "4141414141414141",
+          SSN_1: "123-45-6789",
+          PHONE_NUMBER_1: "555-555-5555",
+          PERSON_1: "Ralph",
+          LOCATION_1: "Boston"
+        })
+        expect(result.input).to eq(input)
+      end
+
+      it "overrides existing NER filters" do
+        score = 0.25
+        ralph = build_entity(text: "Ralph", tag: :person, score:)
+        stub_ner_entities(ralph, boston)
+
+        input = <<~TEXT
+          My name is Ralph
+          My location is Boston
+          My email address is user@example.com
+          My credit card numbers are 4242-4242-4242-4242 and 4141414141414141
+          My social security number is 123-45-6789
+          My phone number is 555-555-5555
+        TEXT
+
+        result = TopSecret::Text.filter(input, filters: {
+          people_filter: TopSecret::Filters::NER.new(
+            label: "NAME",
+            tag: :person,
+            min_confidence_score: score
+          )
+        })
+
+        expect(result.output).to eq(<<~TEXT)
+          My name is [NAME_1]
+          My location is [LOCATION_1]
+          My email address is [EMAIL_1]
+          My credit card numbers are [CREDIT_CARD_1] and [CREDIT_CARD_2]
+          My social security number is [SSN_1]
+          My phone number is [PHONE_NUMBER_1]
+        TEXT
+        expect(result.mapping).to eq({
+          EMAIL_1: "user@example.com",
+          CREDIT_CARD_1: "4242-4242-4242-4242",
+          CREDIT_CARD_2: "4141414141414141",
+          SSN_1: "123-45-6789",
+          PHONE_NUMBER_1: "555-555-5555",
+          NAME_1: "Ralph",
+          LOCATION_1: "Boston"
+        })
+        expect(result.input).to eq(input)
+      end
+
+      it "ignores existing filters" do
+        input = <<~TEXT
+          My name is Ralph
+          My location is Boston
+          My email address is user@example.com
+          My credit card numbers are 4242-4242-4242-4242 and 4141414141414141
+          My social security number is 123-45-6789
+          My phone number is 555-555-5555
+        TEXT
+
+        result = TopSecret::Text.filter(input, filters: {
+          email_filter: nil
+        })
+
+        expect(result.output).to eq(<<~TEXT)
+          My name is [PERSON_1]
+          My location is [LOCATION_1]
+          My email address is user@example.com
+          My credit card numbers are [CREDIT_CARD_1] and [CREDIT_CARD_2]
+          My social security number is [SSN_1]
+          My phone number is [PHONE_NUMBER_1]
+        TEXT
+        expect(result.mapping).to eq({
+          CREDIT_CARD_1: "4242-4242-4242-4242",
+          CREDIT_CARD_2: "4141414141414141",
+          SSN_1: "123-45-6789",
+          PHONE_NUMBER_1: "555-555-5555",
+          PERSON_1: "Ralph",
+          LOCATION_1: "Boston"
+        })
+        expect(result.input).to eq(input)
+      end
+
+      it "respects new regex filters" do
+        input = <<~TEXT
+          My name is Ralph
+          My location is Boston
+          My email address is user@example.com
+          My credit card numbers are 4242-4242-4242-4242 and 4141414141414141
+          My social security number is 123-45-6789
+          My phone number is 555-555-5555
+          My IP address is 192.168.1.1
+        TEXT
+
+        result = TopSecret::Text.filter(input, filters: {
+          ip_address_filter: TopSecret::Filters::Regex.new(
+            label: "IP_ADDRESS",
+            regex: /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
+          )
+        })
+
+        expect(result.output).to eq(<<~TEXT)
+          My name is [PERSON_1]
+          My location is [LOCATION_1]
+          My email address is [EMAIL_1]
+          My credit card numbers are [CREDIT_CARD_1] and [CREDIT_CARD_2]
+          My social security number is [SSN_1]
+          My phone number is [PHONE_NUMBER_1]
+          My IP address is [IP_ADDRESS_1]
+        TEXT
+        expect(result.mapping).to eq({
+          EMAIL_1: "user@example.com",
+          CREDIT_CARD_1: "4242-4242-4242-4242",
+          CREDIT_CARD_2: "4141414141414141",
+          SSN_1: "123-45-6789",
+          PHONE_NUMBER_1: "555-555-5555",
+          PERSON_1: "Ralph",
+          LOCATION_1: "Boston",
+          IP_ADDRESS_1: "192.168.1.1"
+        })
+        expect(result.input).to eq(input)
+      end
+
+      it "respects new NER filters" do
+        ip_address = build_entity(text: "192.168.1.1", tag: :ip_address)
+        stub_ner_entities(ralph, boston, ip_address)
+
+        input = <<~TEXT
+          My name is Ralph
+          My location is Boston
+          My email address is user@example.com
+          My credit card numbers are 4242-4242-4242-4242 and 4141414141414141
+          My social security number is 123-45-6789
+          My phone number is 555-555-5555
+          My IP address is 192.168.1.1
+        TEXT
+
+        result = TopSecret::Text.filter(input, filters: {
+          ip_address_filter: TopSecret::Filters::NER.new(
+            label: "IP_ADDRESS",
+            tag: :ip_address
+          )
+        })
+
+        expect(result.output).to eq(<<~TEXT)
+          My name is [PERSON_1]
+          My location is [LOCATION_1]
+          My email address is [EMAIL_1]
+          My credit card numbers are [CREDIT_CARD_1] and [CREDIT_CARD_2]
+          My social security number is [SSN_1]
+          My phone number is [PHONE_NUMBER_1]
+          My IP address is [IP_ADDRESS_1]
+        TEXT
+        expect(result.mapping).to eq({
+          EMAIL_1: "user@example.com",
+          CREDIT_CARD_1: "4242-4242-4242-4242",
+          CREDIT_CARD_2: "4141414141414141",
+          SSN_1: "123-45-6789",
+          PHONE_NUMBER_1: "555-555-5555",
+          PERSON_1: "Ralph",
+          LOCATION_1: "Boston",
+          IP_ADDRESS_1: "192.168.1.1"
+        })
+        expect(result.input).to eq(input)
+      end
     end
 
     it "removes duplicate entries from the mapping" do
@@ -339,17 +610,17 @@ RSpec.describe TopSecret::Text do
       end
     end
   end
+end
 
-  private
+private
 
-  def build_entity(text:, tag:, score: TopSecret.min_confidence_score)
-    {text:, tag: tag.to_s.upcase, score:}
-  end
+def build_entity(text:, tag:, score: TopSecret.min_confidence_score)
+  {text:, tag: tag.to_s.upcase, score:}
+end
 
-  def stub_ner_entities(*entities)
-    doc = instance_double("Mitie::Document", entities:)
-    ner = instance_double("Mitie::NER", doc:)
+def stub_ner_entities(*entities)
+  doc = instance_double("Mitie::Document", entities:)
+  ner = instance_double("Mitie::NER", doc:)
 
-    stub_const("Mitie::NER", class_double("Mitie::NER", new: ner))
-  end
+  stub_const("Mitie::NER", class_double("Mitie::NER", new: ner))
 end
