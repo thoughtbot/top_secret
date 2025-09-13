c31e42b367440d17c7e8fc7d5ffd204fa5eb00e6
Steve Polito <stevepolito@hey.com>
2025-09-13 05:51:39 -0400

    WIP

--- lib/top_secret/text/global_mapping.rb
+++ lib/top_secret/text/global_mapping.rb
@@ -4,26 +4,41 @@ module TopSecret
   class Text
     class GlobalMapping
       def self.from_results(individual_results)
-        global_mapping = {}
-        label_counters = {}
+        new.build_from_results(individual_results)
+      end
 
-        individual_results.each do |result|
-          result.mapping.each do |individual_key, value|
-            next if global_mapping.key?(value)
+      def initialize
+        @mapping = {}
+        @label_counters = {}
+      end
 
-            # TODO: This assumes labels are formatted consistently.
-            # We need to account for the following for the case where a label could begin with an "_"
-            label_type = individual_key.to_s.rpartition("_").first
+      def build_from_results(individual_results)
+        individual_results.each { |result| process_result(result) }
 
-            label_counters[label_type] ||= 0
-            label_counters[label_type] += 1
-            global_key = :"#{label_type}_#{label_counters[label_type]}"
+        mapping
+      end
 
-            global_mapping[value] = global_key
-          end
+      private
+
+      attr_accessor :mapping
+      attr_reader :label_counters
+
+      def process_result(result)
+        result.mapping.each do |individual_key, value|
+          next if mapping.key?(value)
+
+          mapping[value] = generate_global_key(individual_key)
         end
+      end
 
-        global_mapping
+      def generate_global_key(individual_key)
+        # TODO: This assumes labels are formatted consistently.
+        # We need to account for the following for the case where a label could begin with an "_"
+        label_type = individual_key.to_s.rpartition("_").first
+
+        label_counters[label_type] ||= 0
+        label_counters[label_type] += 1
+        :"#{label_type}_#{label_counters[label_type]}"
       end
     end
   end
