7d84c4e064a6373e794598a68adb78e71662a6d8
Steve Polito <stevepolito@hey.com>
2025-09-26 10:40:49 -0400

    wip

--- lib/top_secret/mapping.rb
+++ lib/top_secret/mapping.rb
@@ -15,34 +15,20 @@ module TopSecret
     end
 
     def method_missing(method_name)
-      types = mapping.keys.map do |key|
-        key.to_s.split("_").first.downcase
-      end
-
-      mapping_methods = types.uniq.map do |type|
-        (type + "_mapping").to_sym
-      end
-
-      plural_types = types.uniq.map(&:pluralize).map(&:to_sym)
-
-      predicate_types = plural_types.map do |plural_type|
-        (plural_type.to_s + "?").to_sym
-      end
-
       if mapping_methods.select { _1 == method_name }.first
         self.class.define_method(method_name) do
           mapping.select { |key, _| key.start_with? method_name.to_s.split("mapping").first.upcase }
         end
 
         send(method_name)
-      elsif plural_types.select { _1 == method_name }.first
+      elsif pluralized_methods.select { _1 == method_name }.first
         mapping_method = (method_name.to_s.singularize + "_mapping").to_sym
         self.class.define_method(method_name) do
           send(mapping_method).values
         end
 
         send(method_name)
-      elsif predicate_types.select { _1 == method_name }.first
+      elsif predicate_methods.select { _1 == method_name }.first
         plural_method = method_name.to_s.chomp("?").to_sym
         self.class.define_method(method_name) do
           send(plural_method).any?
@@ -51,6 +37,30 @@ module TopSecret
         send(method_name)
       else
         super
+      end
+    end
+
+    private
+
+    def types
+      @types ||= mapping.keys.map do |key|
+        key.to_s.split("_").first.downcase
+      end
+    end
+
+    def pluralized_methods
+      @pluralized_methods ||= types.uniq.map(&:pluralize).map(&:to_sym)
+    end
+
+    def predicate_methods
+      @predicate_methods ||= pluralized_methods.map do |plural_type|
+        (plural_type.to_s + "?").to_sym
+      end
+    end
+
+    def mapping_methods
+      @mapping_methods ||= types.uniq.map do |type|
+        (type + "_mapping").to_sym
       end
     end
   end
