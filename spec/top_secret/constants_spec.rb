af57b251f23a1d274dfe6e6b923a842bf1e53da7
tleish <tleish@users.noreply.github.com>
2025-09-28 10:54:58 -0600

    reformat test back to how it was originaly

--- spec/top_secret/constants_spec.rb
+++ spec/top_secret/constants_spec.rb
@@ -1,28 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe "TopSecret::PHONE_REGEX" do
-  describe "common formats" do
-    phone_numbers = [
-      "+1 415-555-1234",
-      "(415) 555-1234",
-      "415.555.1234",
-      "415 555 1234",
-      "415-555-1234"
-    ]
+  phone_numbers = [
+    "+1 415-555-1234",
+    "(415) 555-1234",
+    "415.555.1234",
+    "415 555 1234",
+    "415-555-1234"
+  ]
 
-    phone_numbers.each do |phone_number|
-      it "matches #{phone_number}" do
-        expect(phone_number).to match(TopSecret::PHONE_REGEX)
-      end
+  phone_numbers.each do |phone_number|
+    it "matches #{phone_number}" do
+      expect(phone_number).to match(TopSecret::PHONE_REGEX)
     end
+  end
 
-    it "matches embedded parenthesized number and preserves mapping" do
-      input  = "My phone number is (555) 555-5555"
-      output = input.gsub(TopSecret::PHONE_REGEX) { "[PHONE_NUMBER_1]" }
-      md     = input.match(TopSecret::PHONE_REGEX)
+  it "matches embedded parenthesized number and preserves mapping" do
+    input  = "My phone number is (555) 555-5555"
+    output = input.gsub(TopSecret::PHONE_REGEX) { "[PHONE_NUMBER_1]" }
+    md     = input.match(TopSecret::PHONE_REGEX)
 
-      expect(output).to eq("My phone number is [PHONE_NUMBER_1]")
-      expect(md[0]).to eq("(555) 555-5555")
-    end
+    expect(output).to eq("My phone number is [PHONE_NUMBER_1]")
+    expect(md[0]).to eq("(555) 555-5555")
   end
 end
