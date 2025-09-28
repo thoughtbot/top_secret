62e91dc777b8e092ed7d58b54c7dbb6065993524
tleish <tleish@users.noreply.github.com>
2025-09-28 10:42:52 -0600

    add failing test for embedded parenthesized number and preserves mapping

--- spec/top_secret/constants_spec.rb
+++ spec/top_secret/constants_spec.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe "TopSecret::PHONE_REGEX" do
-  phone_numbers = [
-    "+1 415-555-1234",
-    "(415) 555-1234",
-    "415.555.1234",
-    "415 555 1234",
-    "415-555-1234"
-  ]
+  describe "common formats" do
+    phone_numbers = [
+      "+1 415-555-1234",
+      "(415) 555-1234",
+      "415.555.1234",
+      "415 555 1234",
+      "415-555-1234"
+    ]
 
-  phone_numbers.each do |phone_number|
-    it "matches #{phone_number}" do
-      expect(phone_number).to match(TopSecret::PHONE_REGEX)
+    phone_numbers.each do |phone_number|
+      it "matches #{phone_number}" do
+        expect(phone_number).to match(TopSecret::PHONE_REGEX)
+      end
     end
+  end
+
+  it "matches embedded parenthesized number and preserves mapping" do
+    input  = "My phone number is (555) 555-5555"
+    output = input.gsub(TopSecret::PHONE_REGEX) { "[PHONE_NUMBER_1]" }
+    md     = input.match(TopSecret::PHONE_REGEX)
+
+    expect(output).to eq("My phone number is [PHONE_NUMBER_1]")
+    expect(md[0]).to eq("(555) 555-5555")
   end
 end
